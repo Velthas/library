992b2850432e3122f083a358c9423db111527a67
Velthas <damianobattaglia97@gmail.com>
2022-01-05 20:22:44 +0100

    Basic HTML and Script structure

---
+++ script.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+//Constructor for books
+function Book (name, pages, read) {
+    this.name = name;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary () {
+    //TODO: Get the information from a form and use it to create a book, then use that info to create a book card
+}
